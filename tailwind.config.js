7b7ec392fccb5184cdcd907e80b0962b02025631
sjoerdoudman <sjoerdoudman@users.noreply.github.com>
2022-09-06 15:20:36 +0200

    Forked repo
    Making franky

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ["./**/*.liquid"],
+  },
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
